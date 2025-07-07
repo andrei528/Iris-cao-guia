b9180625f7112eda713a9b060ab8a0cef35e660a
Andrei Martinez <143437790+andrei528@users.noreply.github.com>
2025-07-07 16:47:26 -0300

    Delete Iris-Cao directory

--- Iris-Cao/JS/formulario.js
+++
@@ -1,7 +0,0 @@
-const inputCPF = document.getElementById("cpf");
-
-inputCPF.addEventListener("input", function (e) {
-    // Remove qualquer caractere que não seja número
-    let valor = e.target.value.replace(/\D/g, "");
-    e.target.value = valor;
-});
